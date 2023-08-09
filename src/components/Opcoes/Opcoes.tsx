2b297e07decfe6a5e46eb2aba77b2e37afad1e79
Jean Carlos <jean@naxostelecom.com.br>
2023-08-09 11:23:30 -0300

    insida função no componente opçoes

---
+++ src/components/Opcoes/Opcoes.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import * as React from "react";
+import styles from "./SideBar.module.css";
+import RootLayout from "@/app/layout";
+import Provisionamento from "@/pages/provisionamento";
+import NavBar from "../NavBar/NavBar";
+
+export default function Opcoes() {
+    return (
+        <>
+        </>
+    );
+}
