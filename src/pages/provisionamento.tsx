57f1eac8b2b61c1c33034048d3fdbc72fecbfdf2
Jean Carlos <jean@naxostelecom.com.br>
2023-12-12 14:45:40 -0300

    insiro titulo nas paginas

--- src/pages/provisionamento.tsx
+++ src/pages/provisionamento.tsx
@@ -1,9 +1,12 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import SideBar from "@/components/SideBar/SideBar";
 import FormProvisionamento from "@/components/FormProvisionamento/FormProvisionamento";
 
 const Provisionamento = () => {
+  useEffect(() => {
+    document.title = "Provisionamento FTTH Naxos Telecom - Provisionamento"
+  })
   return (
     <>
       <SideBar></SideBar>
--- src/pages/vlan.tsx
+++ src/pages/vlan.tsx
@@ -1,10 +1,13 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import SideBar from "@/components/SideBar/SideBar";
 import tokenVerify from "@/api/middleware/tokenVerify";
 import FormVlan from "@/components/FormVlan/FormVlan";
 
 const Vlan = () => {
+  useEffect(() => {
+    document.title = "Provisionamento FTTH Naxos Telecom - Vlan"
+  })
   tokenVerify()
   return (
     <>
