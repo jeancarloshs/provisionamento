e76fbeef8eb26714a6d11b43ff3daa31f7def1bd
Jean <jeancarlos.na@hotmail.com>
2023-12-19 22:25:30 -0300

    componentizado as paginas telefonia, vlan, bridge

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,19 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { StrictMode, useEffect } from "react";
 import SideBar from "@/components/SideBar/SideBar";
 import tokenVerify from "@/api/middleware/tokenVerify";
 
 const Home = () => {
   // Com o Array vazio ele executa somente 1 vez na tela
   useEffect(() => {
-    document.title = "Provisionamento FTTH Naxos Telecom - Home"
-  }, [])
-  tokenVerify()
+    document.title = "Provisionamento FTTH Naxos Telecom - Home";
+  }, []);
+  tokenVerify();
   return (
     <>
-      <SideBar></SideBar>
+      <StrictMode>
+        <SideBar></SideBar>
+      </StrictMode>
     </>
   );
 };
