4412e860a3c7c7dfb9eb2c47dd4d049de5c7b8be
Jean Carlos <jean@naxostelecom.com.br>
2023-12-22 16:20:08 -0300

    loading opçoes

--- src/components/Opcoes/OpcoesComponent.tsx
+++ src/components/Opcoes/OpcoesComponent.tsx
@@ -1,11 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 import * as React from "react";
 import styles from "./Opcoes.module.css";
-import RootLayout from "@/app/layout";
-import Provisionamento from "@/pages/provisionamento";
-import NavBar from "../NavBar/NavBar";
 import externalTechnician from "@/api/controller/ExternalTechnicianController";
 import deleteUser from "@/api/controller/DeleteUser";
 import userConnected from "@/api/middleware/userConnected";
@@ -55,9 +51,6 @@ export default function OpcoesComponent() {
 
   return (
     <>
-      <div className={styles.btnAdd}>
-        <ButtonComponent btnId="adduser" btnName="Adicionar Usuário" btnOnClick={handleAddButton}>Adicionar</ButtonComponent>
-      </div>
       {/* {loading && (
         <img
           src='/assets/image/dot-revolve.svg'
@@ -67,42 +60,47 @@ export default function OpcoesComponent() {
       )} */}
       {
         userList &&
-        <div className={styles.containerTable}>
-          <table className={styles.table}>
-            <caption className={styles.caption}>USUARIOS</caption>
-            <thead className={styles.thead}>
-              <tr className={styles.tableTr}>
-                <th className={styles.th}>Nome Funcionario</th>
-                <th className={styles.th}>Cargo Funcionario</th>
-                <th className={styles.th}>Permissão do Colaborador</th>
-                <th className={styles.th}>Status do Colaborador</th>
-                <th className={styles.th}>Editar</th>
-              </tr>
-            </thead>
-            <tbody className={styles.tbody}>
-              {
-                userList.map((user: UserList, index) => (
-                  <tr key={index} className={user.status == 0 ? styles.tdUserInactive : styles.td}>
-                    <td className={styles.td}>{user.nomeFuncionario}</td>
-                    <td className={styles.td}>{user.cargoFuncionario}</td>
-                    <td className={styles.td}>{user.permissaoDoColaborador}</td>
-                    <td className={styles.td}>{user.status != 0 ? "Ativo" : "Inativo"}</td>
-                    <td className={`${styles.td} ${styles.tdEdite}`}>
-                      <a href={user.id}>
-                        <img src={imageEdite} alt="Editar" className={styles.imageEdite} />
-                      </a>
-                      {userId == '1' ?
-                        <a href="#" data-confirm="Tem certeza ?" onClick={() => handleUserDelete(user.id)} >
-                          <img src={imageDelete} alt="Excluir" className={styles.imageEdite} />
+        <>
+          <div className={styles.btnAdd}>
+            <ButtonComponent btnId="adduser" btnName="Adicionar Usuário" btnOnClick={handleAddButton}>Adicionar</ButtonComponent>
+          </div>
+          <div className={styles.containerTable}>
+            <table className={styles.table}>
+              <caption className={styles.caption}>USUARIOS</caption>
+              <thead className={styles.thead}>
+                <tr className={styles.tableTr}>
+                  <th className={styles.th}>Nome Funcionario</th>
+                  <th className={styles.th}>Cargo Funcionario</th>
+                  <th className={styles.th}>Permissão do Colaborador</th>
+                  <th className={styles.th}>Status do Colaborador</th>
+                  <th className={styles.th}>Editar</th>
+                </tr>
+              </thead>
+              <tbody className={styles.tbody}>
+                {
+                  userList.map((user: UserList, index) => (
+                    <tr key={index} className={user.status == 0 ? styles.tdUserInactive : styles.td}>
+                      <td className={styles.td}>{user.nomeFuncionario}</td>
+                      <td className={styles.td}>{user.cargoFuncionario}</td>
+                      <td className={styles.td}>{user.permissaoDoColaborador}</td>
+                      <td className={styles.td}>{user.status != 0 ? "Ativo" : "Inativo"}</td>
+                      <td className={`${styles.td} ${styles.tdEdite}`}>
+                        <a href={user.id}>
+                          <img src={imageEdite} alt="Editar" className={styles.imageEdite} />
                         </a>
-                        : ''}
-                    </td>
-                  </tr>
-                ))
-              }
-            </tbody>
-          </table>
-        </div>
+                        {userId == '1' ?
+                          <a href="#" data-confirm="Tem certeza ?" onClick={() => handleUserDelete(user.id)} >
+                            <img src={imageDelete} alt="Excluir" className={styles.imageEdite} />
+                          </a>
+                          : ''}
+                      </td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          </div>
+        </>
       }
     </>
   );
