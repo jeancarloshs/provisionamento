5f7ae1d3b125181754e413e78c918d1bda9f9016
Jean <jeancarlos.na@hotmail.com>
2024-05-19 01:00:30 -0300

    style: ajuste no estilo da tabela

--- src/components/Helpe/Helpe.tsx
+++ src/components/Helpe/Helpe.tsx
@@ -14,8 +14,8 @@ export default function Helpe() {
   const imageEdite = "/assets/image/icons8-maintenance-64.png";
   const imageDelete = "/assets/image/icons8-excluir-16.png";
   const [userData, setUserData] = useState({});
-  const [userId, setUserId] = useState<string>();
-  const [isAdmin, setIsAdmin] = useState<boolean | null>();
+  const [userId, setUserId] = useState<string>("");
+	const [isAdmin, setIsAdmin] = useState<boolean | null>(null);
 
   const usersFetch = async () => {
     try {
@@ -70,11 +70,11 @@ export default function Helpe() {
           <Modal />
         </div>
       ) : (
-        ""
+        null
       )}
       <div className={styles.containerTable}>
         <table className={styles.table}>
-          <caption className={styles.caption}>USUARIOS</caption>
+          <caption className={styles.caption}>Ajuda</caption>
           <thead className={styles.thead}>
             <tr className={styles.tableTr}>
               <th className={styles.th}>Nome Funcionario</th>
@@ -83,37 +83,27 @@ export default function Helpe() {
           </thead>
           <tbody className={styles.tbody}>
             {Array.isArray(files) ? (
-              files?.map((item, index) => (
-                <tr key={index} className="">
+              files.map((item, index) => (
+                <tr key={index} className={styles.td}>
                   <td className={styles.td}>
-                    <a
-                      className={styles.links}
-                      key={item.id}
-                      href={item.url}
-                      target="_blank"
-                    >
-                      {item.nome}
-                    </a>
+                    <a className={styles.links} key={item.id} href={item.url} target="_blank">{item.nome}</a>
                   </td>
                   {userId == "1" ? (
                     <td className={`${styles.td} ${styles.tdEdite}`}>
-                      {/* <Modal userId={user.id} userName={user.nomeFuncionario} userEmail={user.emailFuncionario} userPassword={user.senhaFuncionario} userStatus={user.status} userRole={user.admin} employeePosition={user.cargoFuncionario} hasImage={true} /> */}
+                      {/* <Modal docsId={item.id} docsName={item.nome} docsUrl={item.url} hasImage={true} /> */}
+											<Modal userId={item.id} userName={item.nomeFuncionario} userEmail={item.emailFuncionario} userPassword={item.senhaFuncionario} userStatus={item.status} userRole={item.admin} employeePosition={item.cargoFuncionario} hasImage={true} />
                       <a href="#" data-confirm="Tem certeza ?">
-                        <img
-                          src={imageDelete}
-                          alt="Excluir"
-                          className={styles.imageEdite}
-                        />
+                        <img src={imageDelete} alt="Excluir" className={styles.imageEdite} />
                       </a>
                     </td>
                   ) : (
-                    ""
+                    null
                   )}
                 </tr>
               ))
             ) : (
               <tr>
-                <td className={styles.td}>Nenhum Arquivo Encontrado</td>
+                <td className={`${styles.td} ${styles.tdCenter}`} colSpan={2}>Nenhum Arquivo Encontrado</td>
               </tr>
             )}
           </tbody>
