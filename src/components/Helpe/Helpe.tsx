9efeb5d75971abff2db0d8dc453748db5441eb19
Jean <jeancarlos.na@hotmail.com>
2024-05-19 00:20:08 -0300

    refactor: alterado view de ajuda

--- src/components/Helpe/Helpe.tsx
+++ src/components/Helpe/Helpe.tsx
@@ -2,19 +2,53 @@
 import listFiles from "@/api/controller/ListFilesController";
 import styles from "./Helpe.module.css";
 import { useState, useEffect } from "react";
+import userConnected from "@/api/middleware/userConnected";
+import Modal from "../Modal/Modal";
+import UserLoged from "@/api/controller/UserLogedController";
+import jwt_decode from "jwt-decode";
 
 export default function Helpe() {
   const [token, setToken] = useState<String | null>("");
   const [files, setFiles] = useState<String | null>("");
   const [loading, setLoading] = useState(true);
+  const imageEdite = "/assets/image/icons8-maintenance-64.png";
+  const imageDelete = "/assets/image/icons8-excluir-16.png";
+  const [userData, setUserData] = useState({});
+  const [userId, setUserId] = useState<string>();
+  const [isAdmin, setIsAdmin] = useState<boolean | null>();
+
+  const usersFetch = async () => {
+    try {
+      const userIdFunction = await userConnected();
+      const userArrayId = userIdFunction.data[0]["id"];
+      setUserId(userArrayId);
+      const getToken = sessionStorage.getItem("Token") as string;
+      setToken(getToken);
+
+      if (getToken) {
+        getFiles(getToken);
+      }
+
+      interface DecodedToken {
+        id: number;
+        storedToken: string;
+        userName: string;
+      }
+
+      // Decodifica o token e obtém os dados do usuário
+      const decodedToken: DecodedToken = jwt_decode(getToken);
+      setUserData(decodedToken);
+      const id = decodedToken.id;
+      let userName = await UserLoged(getToken, id);
+      setIsAdmin(userName.data[0].admin);
+      // console.log('admin', userName.data[0].admin);
+    } catch (e) {
+      console.error("ERRO:", e);
+    }
+  };
 
   useEffect(() => {
-    const getToken = sessionStorage.getItem("Token") as string;
-    setToken(getToken);
-
-    if (getToken) {
-      getFiles(getToken);
-    }
+    usersFetch();
   }, []);
 
   const getFiles = async (token: string) => {
@@ -27,30 +61,59 @@ export default function Helpe() {
       console.error("Error fetching files:", error);
       setLoading(false);
     }
-  }
-
-  const servicesTypesOptions: any = Array.isArray(files) ? (
-    files.map((item, index) => (
-      <li key={index}>
-        <a className={styles.links} key={item.id} href={item.url} target="_blank">
-          {item.nome}
-        </a>
-      </li>
-    ))
-  ) : (
-    <li><p>Carregando...</p></li>
-  );
+  };
 
   return (
     <>
-      <div className={styles.main}>
-        {/* {servicesTypesOptions.length > 0 ? ( */}
-          <ul key={servicesTypesOptions.length}>
-            {servicesTypesOptions}
-          </ul>
-        {/* ) : (
-          <p>Nenhum arquivo disponível</p>
-        )} */}
+      {isAdmin == true ? (
+        <div className={styles.btnAdd}>
+          <Modal />
+        </div>
+      ) : (
+        ""
+      )}
+      <div className={styles.containerTable}>
+        <table className={styles.table}>
+          <caption className={styles.caption}>USUARIOS</caption>
+          <thead className={styles.thead}>
+            <tr className={styles.tableTr}>
+              <th className={styles.th}>Nome Funcionario</th>
+              <th className={styles.th}>Editar</th>
+            </tr>
+          </thead>
+          <tbody className={styles.tbody}>
+            {Array.isArray(files) &&
+              files?.map((item, index) => (
+                <tr key={index} className="">
+                  <td className={styles.td}>
+                    <a
+                      className={styles.links}
+                      key={item.id}
+                      href={item.url}
+                      target="_blank"
+                    >
+                      {item.nome}
+                    </a>
+                  </td>
+                  {}
+                  {userId == "1" ? (
+                    <td className={`${styles.td} ${styles.tdEdite}`}>
+                      {/* <Modal userId={user.id} userName={user.nomeFuncionario} userEmail={user.emailFuncionario} userPassword={user.senhaFuncionario} userStatus={user.status} userRole={user.admin} employeePosition={user.cargoFuncionario} hasImage={true} /> */}
+                      <a href="#" data-confirm="Tem certeza ?">
+                        <img
+                          src={imageDelete}
+                          alt="Excluir"
+                          className={styles.imageEdite}
+                        />
+                      </a>
+                    </td>
+                  ) : (
+                    ""
+                  )}
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
